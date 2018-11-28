9924165a70d1ae43e239b249811819b58fa71f0f
shahin1290 <s.patowary@yahoo.com>
2018-11-28 16:59:00 +0100

    Adds functions to check content on page

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -4,9 +4,9 @@ Given('I visit the site', async function () {
     return await this.openHomePage()
 });
 
-Then('I should see {string}', async function (string) {
-// Write code here that turns the phrase above into concrete actions
-return 'pending';
+Then('I should see {string}', async function (content) {
+    return await this.pageHasTextContent(content)
+
 });
 
 After(async function() {
--- features/support/world.js
+++ features/support/world.js
@@ -13,8 +13,16 @@ class RPSWorld {
     this.page = await this.browser.newPage()
     await this.page.goto(HOME_PAGE)
   }
+  
   async closeHomePage() {
     await this.browser.close()
+  }
+
+  async pageHasTextContent(expectedContent) {
+    const pageContent = await this.page.content()
+    const actualContent = pageContent.match(expectedContent)[0]
+
+    expect(actualContent).to.be.eq(expectedContent)
   }
 }
 
